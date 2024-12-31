b4f1acdd63095077a21f656a6a0ad2e6356d3016
Schroeder-R <Schroeder357@gmail.com>
2024-12-31 09:35:58 -0600

    added recusive method to copy contents from static to public from Chapter 5 Lesson 1

--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_contents(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination) # Delete all contents of the destination directory
+    os.mkdir(destination)
+
+    for item in os.listdir(source):
+        source_item = os.path.join(source, item)
+        destination_item = os.path.join(destination, item)
+
+        if os.path.isdir(source_item):
+            # Recursive call for directories
+            copy_contents(source_item, destination_item)
+        else:
+            # Copy files
+            shutil.copy(source_item, destination_item)
+            print(f"Copied file: {source_item} to {destination_item}")
 
 def main():
-    test = TextNode("This is a test", TextType.BOLD, "https://www.boot.dev")
-    print(test)
+    root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    source_dir = os.path.join(root_dir, "static")
+    destination_dir = os.path.join(root_dir, "public")
+    copy_contents(source_dir, destination_dir)
+    
 if __name__ == "__main__":
     main()
+
+
